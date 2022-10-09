dc52dc1ea8612610c803a928e3d9ddee718a6acd
Galalkoro98 <galal.koro@gmail.com>
2022-10-10 00:36:58 +0200

    add new files and exercise done

--- sumAll/function-while-loop.js
+++ sumAll/function-while-loop.js
@@ -1,11 +1,27 @@
-# js-problems
-js mini project to master javascript 
+'use strict';
 
-<code>
 //Write a function called sumAll that takes a number as an argument.
 //This function should sum the numbers up to that number as a result (number inclusive)
 //Number should be bigger than 1 if it is not return "Please enter a number bigger than 1"
 //use while loop
 
 // sumAll (5) => should return 15 => 0+1+2+3+4+5 = 15
-</code>`
+
+
+function sumAll (number){
+    let sum = 0;
+
+    if (number <= 1) {
+        return "please type bigger than 1"
+    }
+
+    while (number >= 0) {
+        sum += number;
+        console.log('+' + number);
+        number--;
+        
+    }
+    return sum;
+}
+
+console.log( '= ' + sumAll(5));

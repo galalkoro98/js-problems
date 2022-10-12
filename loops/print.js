a491545cbfd5a64ed9074578411ccca38fab1b5b
Galalkoro98 <galal.koro@gmail.com>
2022-10-13 00:51:41 +0200

    add new folder

---
+++ loops/print.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let num = '';
+let num1 = ' g ';
+
+for(let i = 10; i <= 30; i++){
+    num += i + num1;
+
+    if (i % 2) {
+        console.log(num);
+    }
+
+    if (i % 3) {
+        console.log(num);
+        
+    }
+
+if (i % 4) {
+    console.log(num);
+    
+}
+
+if (i % 5) {
+    console.log(num);
+    
+}
+
+}
+
+console.log(num);

c652079a4558c30d7592757e35a3a038b1e53dbb
Galalkoro98 <galal.koro@gmail.com>
2022-10-13 23:42:13 +0200

    update and add new files

---
+++ loops/even-sum.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const findEvenSums = (a, b)=>{
+    const maxNumber = Number(Math.max(a, b));
+    const minNumber = Number(Math.min(a, b));
+    let total = 0;
+
+    for(let i = minNumber; i <= maxNumber; i++){
+        if (i % 2 === 0) {
+            console.log(i);
+            total = total + i;
+            
+        }
+    }
+    console.log({total});
+
+    return total;
+
+}
+
+console.log(findEvenSums("14", "2"));
